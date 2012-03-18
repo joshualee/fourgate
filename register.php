64c0fe285e2f05e3ad1ff03706c60ad755f049bf
Joshua Lee <LeeJoshuaK@gmail.com>
2012-03-18 18:31:19 -0400

    login/register

--- register.php
+++ register.php
@@ -13,15 +13,14 @@
     <div data-role="page" id="home"> <!-- home page -->
  
         <div data-role="header">
-        	<div data-role="controlgroup" data-type="horizontal" style="margin-left:75px;margin-top:5px;" >
-							<a data-rel="back" data-icon="back" data-iconpos="notext"></a>
-							<a href="home.php" data-icon="home" data-theme="b" data-iconpos="notext"></a>
-					</div>
+					<a data-rel="back" data-icon="back" data-iconpos="notext"></a>
+					<a href="home.php" data-icon="home" data-theme="b" data-iconpos="notext"></a>
 					<a href="sign_in.php" data-role="button" data-mini="true">Sign In</a>
-            <h1>FOURGATE</h1>
+            <h1>Register</h1>
         </div><!-- /header -->
     
-        <div data-role="content">	
+        <div data-role="content">
+        		The register page has not yet been implemented.
             <a href="#view_portal" data-role="button" data-icon="arrow-r" id="home_suggestions_button">View Portal</a>
             <a href="#hubs_near_me" data-role="button" data-icon="arrow-r" id="home_my_favorites_button">Hubs Near Me</a>
             <a href="#create_hub" data-role="button" data-icon="arrow-plus">Create Hub</a>
---
+++ sign_in.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title>FOURGATE</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
+	<script src="home.js" type="text/javascript"></script>
+    </head> 
+<body> 
+
+    <div data-role="page" id="home"> <!-- home page -->
+ 
+        <div data-role="header">
+					<a data-rel="back" data-icon="back" data-iconpos="notext"></a>
+					<a href="home.php" data-icon="home" data-theme="b" data-iconpos="notext"></a>
+					<a href="sign_in.php" data-role="button" data-mini="true">Sign In</a>
+            <h1>Sign In</h1>
+        </div><!-- /header -->
+    
+        <div data-role="content">
+        	The login feature has not yet been implemented.
+        		<form action="form.php" method="post">
+        			<div data-role="fieldcontain" class="ui-hide-label">
+								<label for="username">Username:</label>
+								<input type="text" name="username" id="username" value="" placeholder="Username"/>
+							</div>
+							<div data-role="fieldcontain" class="ui-hide-label">
+								<label for="username">Password:</label>
+								<input type="password" name="password" id="password" value="" placeholder="Password"/>
+							</div>
+        			<button type="submit" name="submit" value="submit-value">Login</button>
+        		</form>
+        		<a href="register.php" data-theme="b" data-role="button">Register</a>
+        </div><!-- /content -->
+    
+        <div data-role="footer">
+            <h4>JLee, LLuo, ALeigh</h4>
+        </div><!-- /footer -->
+
+    </div><!-- /home page -->
+    
+</body>
+</html>
